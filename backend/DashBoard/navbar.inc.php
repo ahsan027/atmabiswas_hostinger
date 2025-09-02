130eecba75e62feb39068faf9a827e973febd7af
ahsan027 <ahsanauddry.ndc@gmail.com>
2025-09-03 04:07:23 +0600

    C

--- backend/DashBoard/navbar.inc.php
+++ backend/DashBoard/navbar.inc.php
@@ -1,10 +1,6 @@
 <header class="bg-white shadow-sm">
     <div class="px-4 py-3 flex justify-between items-center">
-        <div class="flex items-center md:hidden">
-            <button class="text-gray-500 focus:outline-none">
-                <i class="fas fa-bars"></i>
-            </button>
-        </div>
+
         <div class="flex-1 bg-blue-400 ml-3 md:ml-0 p-1 md:p-3 rounded-xl md:flex md:items-center md:justify-between">
 
             <h1 class="text-xl md:text-2xl ml-2 text-white tracking-wider font-semibold ">ATMABISWAS</h1>
